fd89c33f61286d75f8928ac73639e7d1910c0874
Andrea <andrea-9_4@hotmail.it>
2026-05-29 11:56:34 +0200

    replaced a internal links with  Link tag

--- src/components/CardImageText.jsx
+++ src/components/CardImageText.jsx
@@ -1,6 +1,21 @@
 import "./CardImageText.scss";
+import parse from "html-react-parser";
+import { Link } from "react-router-dom";
 
 export default function CardImageText({ image, text, revert = false }) {
+  const options = {
+    //replace links with <Link> for correct routing
+    replace: (domNode) => {
+      if (domNode.name === "a") {
+        const href = domNode.attribs?.href;
+
+        if (href?.startsWith("/")) {
+          return <Link to={href}>{domNode.children[0].data}</Link>;
+        }
+      }
+    },
+  };
+
   return (
     <div className={`card-image-text ${revert ? "revert" : ""}`}>
       <div className="content">
@@ -10,7 +25,7 @@ export default function CardImageText({ image, text, revert = false }) {
           </div>
         )}
 
-        <p dangerouslySetInnerHTML={{ __html: text }}></p>
+        {text && <p>{parse(text, options)}</p>}
       </div>
     </div>
   );
